9af764f4e705201cdfcb3972333e57f93f5a4328
Kunshang Ji <kunshang.ji@intel.com>
2025-09-29 13:15:10 +0800

    [XPU]Fix xpu spec decoding UTs, avoid using cuda graph (#25847)
    Signed-off-by: Kunshang Ji <kunshang.ji@intel.com>

--- vllm/v1/spec_decode/eagle.py
+++ vllm/v1/spec_decode/eagle.py
@@ -80,13 +80,14 @@ class EagleProposer:
         self.attn_layer_names: list[str] = []
         self.indexer_layer_names: list[str] = []
 
-        self.use_cuda_graph = (self.vllm_config.compilation_config.level
+        self.use_cuda_graph = (not current_platform.is_xpu()
+                               and self.vllm_config.compilation_config.level
                                == CompilationLevel.PIECEWISE and
                                not self.vllm_config.model_config.enforce_eager
                                and not self.speculative_config.enforce_eager)
         self.cudagraph_batch_sizes = list(
-            reversed(
-                self.vllm_config.compilation_config.cudagraph_capture_sizes))
+            reversed(self.vllm_config.compilation_config.
+                     cudagraph_capture_sizes)) if self.use_cuda_graph else []
 
         # persistent buffers for cuda graph
         self.input_ids = torch.zeros(self.max_num_tokens,
